f34ffa8d8082b4f8ef4a8ce0045c80253dfa3369
Mathew Benson <mathew@benson.co.ke>
2026-07-15 03:25:39 +0300

    working july2026

--- lua/benson/plugin_config/lspconfig.lua
+++ lua/benson/plugin_config/lspconfig.lua
@@ -1,18 +1,56 @@
 return {
     {
         'neovim/nvim-lspconfig',
-        dependencies = { 'saghen/blink.cmp'},
-        
-        config = function()
-            
-            local capabilities = require('blink.cmp').get_lsp_capabilities()
 
+        config = function()
             vim.lsp.config('clangd', {
                 cmd = { "clangd", "--experimental-modules-support", "--completion-style=detailed", "--pch-storage=disk" },
-                capabilities = capabilities,
             })
 
-            vim.lsp.config('lua_ls', {capabilities = capabilities})
+            vim.lsp.config('lua_ls', {
+                on_init = function(client)
+                    if client.workspace_folders then
+                        local path = client.workspace_folders[1].name
+                        if
+                            path ~= vim.fn.stdpath('config')
+                            and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+                        then
+                            return
+                        end
+                    end
+
+                    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using (most
+                            -- likely LuaJIT in the case of Neovim)
+                            version = 'LuaJIT',
+                            -- Tell the language server how to find Lua modules same way as Neovim
+                            -- (see `:h lua-module-load`)
+                            path = {
+                                'lua/?.lua',
+                                'lua/?/init.lua',
+                            },
+                        },
+                        -- Make the server aware of Neovim runtime files
+                        workspace = {
+                            checkThirdParty = false,
+                            library = {
+                                vim.env.VIMRUNTIME,
+                                -- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
+                                vim.api.nvim_get_runtime_file("lua/lspconfig", false)[1],
+                            },
+                            -- Or pull in all of 'runtimepath'.
+                            -- NOTE: this is a lot slower and will cause issues when working on
+                            -- your own configuration.
+                            -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+                            -- library = vim.api.nvim_get_runtime_file('', true),
+                        },
+                    })
+                end,
+                settings = {
+                    Lua = {},
+                },
+            })
 
             vim.lsp.enable('clangd')
             vim.lsp.enable('qmlls')
@@ -20,6 +58,8 @@ return {
             vim.lsp.enable('ts_ls')
             vim.lsp.enable('cmake')
             vim.lsp.enable('csharp_ls')
+
+
 
             vim.api.nvim_create_autocmd('LspAttach', {
                 group = vim.api.nvim_create_augroup('my.lsp', {}),
@@ -38,6 +78,13 @@ return {
                             end,
                         })
                     end
+
+            vim.api.nvim_create_autocmd({ "BufWrite", "BufEnter", "InsertLeave" }, {
+                group = lspgroup,
+                callback = function()
+                    vim.diagnostic.setloclist({ open = false })
+                end
+            })
                 end,
             })
         end
--- lua/benson/remap.lua
+++ lua/benson/remap.lua
@@ -1,51 +1,17 @@
-local mapper = require("benson.keymap")
--- Map alt-v in command-line mode to replace the commandline
--- with the Ex command-line beneath the cursor in the buffer
--- mapper.cnoremap("<Esc>v", "<C-\>esubstitute(getline(\'.\'), \'^\s*\(\' . escape(substitute(&commentstring, '%s.*$', '', ''), '*') . '\)*\s*:*' , '', '')<CR>")
---
 -- Go to NetRW file explorer
-mapper.nnoremap("<C-p>", ":Ex<CR>")
+vim.keymap.set("n", "<C-p>", ":Ex<CR>")
 
 -- Use <C-L> to clear search highlighting as well as refresh the screen
-mapper.nnoremap("<C-l>", ":nohlsearch <bar> redraw<CR>")
-mapper.inoremap("<C-l>", "<C-O>:nohlsearch <bar> redraw<CR>")
+vim.keymap.set("n", "<C-l>", ":nohlsearch <bar> redraw<CR>")
+vim.keymap.set("i", "<C-l>", "<C-O>:nohlsearch <bar> redraw<CR>")
 
 --show diagnostics in a float
-mapper.nnoremap("<Leader>ds", ":lua vim.diagnostic.open_float()<CR>")
+vim.keymap.set('n', '<Leader>ds', vim.diagnostic.open_float)
 
 --make
-mapper.nnoremap("<Leader>m", ":make<CR>")
+vim.keymap.set("n", "<Leader>m", ":make<CR>")
 
---Telescope mappings
-mapper.nnoremap("<Leader>fc", ":Telescope find_files cwd=~/.config/nvim<CR>")
-mapper.nnoremap("<Leader>fb", ":Telescope buffers<CR>")
-mapper.nnoremap("<Leader>ff", ":Telescope find_files<CR>")
+-- Telescope mappings
 
 --Terminal mappings
-mapper.tnoremap("<Esc>", "<C-bSlash><C-N>")
-
---Lsp mappings
---mapper.nnoremap("gd", ":lua vim.lsp.buf.definition()<CR>")
---mapper.nnoremap("K", ":lua vim.lsp.buf.hover()<CR>")
---mapper.nnoremap("gD", ":lua vim.lsp.buf.implementation()<CR>")
---mapper.nnoremap("<c-k>", ":lua vim.lsp.buf.signature_help()<CR>")
---mapper.nnoremap("1gD", ":lua vim.lsp.buf.type_definition()<CR>")
---mapper.nnoremap("gr", ":lua vim.lsp.buf.references()<CR>")
---mapper.nnoremap("g0", ":lua vim.lsp.buf.document_symbol()<CR>")
---mapper.nnoremap("gW", ":lua vim.lsp.buf.workspace_symbol()<CR>")
---mapper.nnoremap("<Leader>gd", ":lua vim.lsp.buf.declaration()<CR>")
---mapper.nnoremap("<Leader>rn", ":lua vim.lsp.buf.rename()<CR>")
---mapper.nnoremap("<Leader>ca", ":lua vim.lsp.buf.code_action()<CR>")
---mapper.nnoremap("<Leader>cf", ":lua vim.lsp.buf.format( { async = true} )<CR>")
-
-
--- locationlist mapping
---mapper.nnoremap("<Leader>o", ":lopen<CR>")
---mapper.nnoremap("<Leader>]", ":lnext<CR>")
---mapper.nnoremap("<Leader>[", ":lprev<CR>")
---
----- QuickFixlist mapping
---mapper.nnoremap("<C-T>", ":copen<CR>")
---mapper.nnoremap("<C-H>", ":cclose<CR>")
---mapper.nnoremap("<C-]>", ":cnext<CR>")
---mapper.nnoremap("<C-[>", ":cprev<CR>")
+vim.keymap.set("t", "<Esc>", "<C-bSlash><C-N>")
